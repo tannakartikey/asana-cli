8f4b51ad7361a1d5f99677a0ade509728f398942
Kartikey Tanna <tannakartikey@gmail.com>
2017-02-04 02:15:27 +0530

    removed default test

--- spec/asana/cli_spec.rb
+++ spec/asana/cli_spec.rb
@@ -4,8 +4,5 @@ RSpec.describe Asana::Cli do
   it "has a version number" do
     expect(Asana::Cli::VERSION).not_to be nil
   end
-
-  it "does something useful" do
-    expect(false).to eq(true)
-  end
+  
 end
